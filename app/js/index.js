068831a72e3f7eba16a61e437740dba8aa4645f0
Fachrul <ffaathirullah1@gmail.com>
2020-08-19 02:39:57 +0700

    spread

--- app/js/index.js
+++ app/js/index.js
@@ -7,13 +7,34 @@ import { log, logTitle } from 'logger';
 
 /* your imports */
 
-logTitle('Template Literal');
+logTitle('Spread_Array');
 /* coding examples */
 // callback adalah function di dalam function
 
-const name = "Anna";
-const asal = "Bandung";
-const umur = 19;
 
-log(`Nama ${name.length} asal ${asal} umur ${umur}`);
+const array1 = ["Fcahrul", "Faathir", "Rullah"];
+const array2 = ["agus", "asep", "udin"];
 
+const concatArray = [...array1, ...array2];
+
+concatArray.forEach(function(name){
+    log(name);
+});
+
+const name = "fachrul";
+
+const namanya = [...name];
+
+namanya.forEach(function(namew){
+    log(namew);
+})
+
+const addNumbers = function(n1,n2,n3){
+    return n1 + n2 + n3;
+}
+
+const number = [1,2,4];
+
+const addition= addNumbers(...number);
+
+log(addition);
